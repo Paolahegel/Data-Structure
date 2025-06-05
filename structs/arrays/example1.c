2f72807609deeb1862280b90ddefb53cbee603d3
paolamoura <paolamoura.kj@hotmail.com>
2025-06-05 20:52:24 -0300

    feat: update Aluno struct array size and enhance validation function

--- structs/arrays/example1.c
+++ structs/arrays/example1.c
@@ -12,14 +12,38 @@ typedef struct {
 } Aluno;
 
 // created array of 10 Aluno structs
-Aluno alunos_lista[3];
+Aluno alunos_lista[1];
+
+void validaRegistro(Aluno aluno) {
+    printf("\n Digite um nome: ");
+    scanf("%s%*c", aluno.nome);
+
+    int i, posicao = 0;
+
+    for(i = 0; i<1; i++) {
+        if(strcmp(alunos_lista[i].nome, aluno.nome) == 0) {
+            printf("Aluno encontrado! \n");
+            posicao = i;
+        } else {
+            posicao = -1;
+        }
+    }
+
+    if(posicao = -1) {
+        printf("Aluno não encontrado! \n");
+    } else {
+        printf("Aluno encontrado na posição %d \n", posicao);
+    }
+    // This function can be used to validate the student record
+    // For now, it does nothing
+}
 
 int main() {
     int i;
 
     /*populating the arrays of structs with data inserted by the user
      it's necessarry a loop for */
-    for (i = 0; i < 3; i++) {
+    for (i = 0; i < 1; i++) {
         printf("\nDigite o nome do aluno %d: ", i + 1);
         fgets(alunos_lista[i].nome, sizeof(alunos_lista[i].nome), stdin);
         alunos_lista[i].nome[strcspn(alunos_lista[i].nome, "\n")] = 0; // remove newline character
@@ -36,13 +60,15 @@ int main() {
         alunos_lista[i].curso[strcspn(alunos_lista[i].curso, "\n")] = 0; // remove newline character
 }
 
-for(i=0; i< 3; i++) {
+for(i=0; i< 1; i++) {
     printf("\nAluno %d:\n", i + 1);
     printf("Nome: %s\n", alunos_lista[i].nome);
     printf("Idade: %d\n", alunos_lista[i].idade);
     printf("Matrícula: %d\n", alunos_lista[i].matricula);
     printf("Curso: %s\n", alunos_lista[i].curso);
 }
+
+    validaRegistro(alunos_lista[0]); // Call the validation function with the first student
 
     return 0;
 }
